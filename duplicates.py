989ff0eff28cc188c98c166d583d2e24b2e8dc93
ram0973 <ram0973you@gmail.com>
2016-10-05 00:00:42 +0300

    done

--- duplicates.py
+++ duplicates.py
@@ -21,13 +21,13 @@ if __name__ == '__main__':
             path = os.path.join(dir, filename)
             file_names.append(path)
 
-    duplicates = []
+    duplicates = set()
     for el1 in file_names:
         for el2 in file_names:
             if el1 != el2:
                 if are_files_duplicates(el1, el2):
-                    duplicates.append(el1)
-                    duplicates.append(el2)
+                    duplicates.add(el1)
+                    duplicates.add(el2)
 
-    duplicates.sort()
-    print(duplicates)
+    for el in (sorted(duplicates)):
+        print(el)
