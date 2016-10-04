b83d8853020eb9b9846779b22f48d7f58664d931
ram0973 <ram0973you@gmail.com>
2016-10-04 23:34:17 +0300

    done

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,33 @@
+import os
+import sys
 
 
 def are_files_duplicates(file_path1, file_path_2):
-    pass
+    if os.path.getsize(file_path1) == os.path.getsize(file_path_2) and \
+       os.path.split(file_path1)[1] == os.path.split(file_path_2)[1]:
+            return True
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        root_dir = sys.argv[1]
+    else:
+        print('Введите имя папки как аргумент')
+        exit()
+
+    file_names = []
+    for dir, subdirs, files in os.walk(root_dir):
+        for filename in files:
+            path = os.path.join(dir, filename)
+            file_names.append(path)
+
+    duplicates = []
+    for el1 in file_names:
+        for el2 in file_names:
+            if el1 != el2:
+                if are_files_duplicates(el1, el2):
+                    duplicates.append(el1)
+                    duplicates.append(el2)
+
+    duplicates.sort()
+    print(duplicates)
