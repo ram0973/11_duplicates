0a6e223f314f513738e1661aa71b175111e700aa
ram0973 <ram0973@gmail.com>
2016-10-07 00:23:54 +0300

    another algorithm

--- duplicates.py
+++ duplicates.py
@@ -4,40 +4,38 @@ import sys
 import argparse
 
 
-def create_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dir')
-
-    return parser
-
-
-def are_files_duplicates(file_path1, file_path_2):
-    if os.path.getsize(file_path1) == os.path.getsize(file_path_2) and \
-       os.path.split(file_path1)[1] == os.path.split(file_path_2)[1]:
-            return True
-
-if __name__ == '__main__':
+def get_named_argument(arg_name: str) -> str:
     if len(sys.argv) > 1:
-        parser = create_parser()
-        root_dir = parser.parse_args(sys.argv[1:]).dir
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--' + arg_name)
+        return getattr(parser.parse_args(sys.argv[1:]), arg_name)
     else:
-        print('Введите имя папки в формате --dir=ИмяПапки')
+        print('Введите параметр в формате --%s Значение' % arg_name)
         exit(1)
 
-    # может, лучше генератор? чтобы память не кушать
-    # root_dir - может быть не инициализирован
-    file_names = []
+
+def find_duplicates(root_dir: str):
+    duplicates = {}
     for dir, subdirs, files in os.walk(root_dir):
         for filename in files:
             path = os.path.join(dir, filename)
-            file_names.append(path)
+            size = os.path.getsize(path)
+            if filename+str(size) not in duplicates:
+                duplicates[filename+str(size)] = [path]
+            else:
+                duplicates[filename+str(size)].append(path)
+    duplicates = list(filter(lambda x: len(x) > 1, duplicates.values()))
+    return sorted([item for sublist in duplicates for item in sublist])
 
-    duplicates = set()
-    for el1 in file_names:
-        for el2 in file_names:
-            if el1 != el2 and are_files_duplicates(el1, el2):
-                duplicates.add(el1)
-                duplicates.add(el2)
 
-    for el in (sorted(duplicates)):
+if __name__ == '__main__':
+    if sys.platform == 'win32':
+        import win_unicode_console
+        win_unicode_console.enable()
+
+    root_dir = get_named_argument('dir')
+
+    duplicates = find_duplicates(root_dir)
+
+    for el in duplicates:
         print(el)
