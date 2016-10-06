b3459e8259c02626b770de4c8d25d80d70848550
Ramil Yabbarov <ram0973@gmail.com>
2016-10-06 12:44:32 +0300

    changed to named parameter

--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import argparse
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir')
+
+    return parser
 
 
 def are_files_duplicates(file_path1, file_path_2):
@@ -8,21 +16,23 @@ def are_files_duplicates(file_path1, file_path_2):
        os.path.split(file_path1)[1] == os.path.split(file_path_2)[1]:
             return True
 
-
 if __name__ == '__main__':
+    # ошибка, нужен именованный параметр
     if len(sys.argv) > 1:
-        root_dir = sys.argv[1]
+        parser = create_parser()
+        root_dir = parser.parse_args(sys.argv[1:]).dir
     else:
-        print('Введите имя папки как аргумент')
-        exit()
+        print('Введите имя папки в формате --dir=ИмяПапки')
+        exit(1)
 
+    # может, лучше генератор? чтобы память не кушать
+    # root_dir - может быть не инициализирован
     file_names = []
     for dir, subdirs, files in os.walk(root_dir):
         for filename in files:
             path = os.path.join(dir, filename)
             file_names.append(path)
 
-    #
     duplicates = set()
     for el1 in file_names:
         for el2 in file_names:
